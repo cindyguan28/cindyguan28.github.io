43b7e74d099bff7a7a6dea50355a09c6cf806ea5
cindyguan28 <64539283+cindyguan28@users.noreply.github.com>
2020-05-17 17:17:11 +0200

    Create script.js

---
+++ Coursera/module4-solution/js/script.js
@@ -0,0 +1,15 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+  if (firstLetter === 'j') {
+    byeSpeaker.SpeakGoodBye(names[i]);
+  } else {
+    helloSpeaker.SpeakHello(names[i]);
+  }
+}
+
+})(name);
