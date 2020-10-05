91e3b25c0f89d53f37ae3489a22de5624a761401
cindyguan28 <64539283+cindyguan28@users.noreply.github.com>
2020-10-05 10:16:42 +0200

    Create buecherregal.java

---
+++ LOOP/5.2 Aufgaben/buecherregal.java
@@ -0,0 +1,44 @@
+class Buecherregal {
+    private Buch[] regal;
+
+    // Fuegen das neue Attribut hier ein
+    
+        int regal [] kapa = new int [index];
+        int aktuell = 
+        int kapazitaet (int regal){
+            int index = 0;
+            for (int i =0; i<=regal.length; i++){
+                index ++;
+            }
+        }
+        
+        
+        int anzahlBuecher[]  = new int [];
+        int a = aktuell
+        for (int j=0; j<= a.length;j++){
+            anzahlBuecher[j]=a[j];
+        }
+        
+        
+    // Fuegen Sie die neuen Methoden hier ein
+        kapazitaet(){
+            
+        }
+
+    public static void main(String[] args) {
+        Buecherregal br = new Buecherregal();
+        Buch b1 = new Buch("Ein Freund fuer Nanoka", "Saro Tekkotsu");
+        Buch b2 = new Buch("2 Freunde fuer 4 Pfoten", "Michael Ende");
+        Buch b3 = new Buch("Die 3 ???", "Robert Arthur");
+        Buch b4 = new Buch("TKKG", "Stefan Wolf");
+        Buch b5 = new Buch("Die 5 Freunde", "Enid Blyton");
+
+        //br.buchEinstellen(b1);
+        //br.buchEinstellen(b2);
+        //br.buchEinstellen(b3);
+        //br.buchEinstellen(b4);
+        //br.buchEinstellen(b5);
+        //Buch b6 = br.buchMitTitelFinden("Ein Freund fuer Nanoka");
+        //System.out.println(b2.autorAusgeben());
+    }
+}
