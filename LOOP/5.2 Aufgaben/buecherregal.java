565a5dc3e47bf84c66feb3a1b70e9c28ffff1a08
cindyguan28 <64539283+cindyguan28@users.noreply.github.com>
2020-10-12 10:27:24 +0200

    Update buecherregal.java

--- LOOP/5.2 Aufgaben/buecherregal.java
+++ LOOP/5.2 Aufgaben/buecherregal.java
@@ -1,30 +1,79 @@
 class Buecherregal {
     private Buch[] regal;
-
-    // Fuegen das neue Attribut hier ein
+    private int size = 2;
+    private int anzahl;
     
-        int regal [] kapa = new int [index];
-        int aktuell = 
-        int kapazitaet (int regal){
-            int index = 0;
-            for (int i =0; i<=regal.length; i++){
-                index ++;
-            }
-        }
-        
-        
-        int anzahlBuecher[]  = new int [];
-        int a = aktuell
-        for (int j=0; j<= a.length;j++){
-            anzahlBuecher[j]=a[j];
-        }
-        
-        
-    // Fuegen Sie die neuen Methoden hier ein
-        kapazitaet(){
-            
+    public Buecherregal(){
+        regal = new Buch [size];
+        anzahl = 0;
+    }
+
+    int [] aktuellKapa = new int [size];
+    public int kapazitaet (){
+        return regal.length;
         }
 
+    public boolean istVoll(){
+        if (anzahl == regal.length){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+   
+
+    
+    void erweitere(){
+        int sizeNew = anzahl*2;
+         Buch []regalNeu = new Buch [sizeNew];
+            for (int a = 0; a<anzahl; a++){
+                regalNeu [a] = regal[a];
+            }
+            regal = regalNeu;
+    
+
+        }
+        
+//buchEinstellen
+    void buchEinstellen(Buch b){
+        if(istVoll()){
+            erweitere();
+        }
+            regal[anzahl] = b;
+            anzahl++;
+    }
+     
+
+    public int anzahlBuecher(){
+       return anzahl; 
+    }
+    
+    public Buch buchMitTitelFinden (String titel){
+        for(int index=0;index <anzahl; index++){
+            if(regal[index].titelAusgeben().equals(titel)){
+                //Buch b = regal[index];
+                return regal[index];
+            }
+         }
+        return null;
+    }
+
+
+    void buchMitTitelEntfernen (String titel){
+        for(int index=0;index <anzahl; index++){
+            if(regal[index].titelAusgeben().equals(titel)){
+                Buch b = regal[index];
+                regal[index] = regal[anzahl - 1];
+                anzahl--;
+                
+            //return regal[index];
+            }
+        //return null;
+        }
+    }
+                    
+     
     public static void main(String[] args) {
         Buecherregal br = new Buecherregal();
         Buch b1 = new Buch("Ein Freund fuer Nanoka", "Saro Tekkotsu");
@@ -33,12 +82,12 @@ class Buecherregal {
         Buch b4 = new Buch("TKKG", "Stefan Wolf");
         Buch b5 = new Buch("Die 5 Freunde", "Enid Blyton");
 
-        //br.buchEinstellen(b1);
-        //br.buchEinstellen(b2);
-        //br.buchEinstellen(b3);
-        //br.buchEinstellen(b4);
-        //br.buchEinstellen(b5);
-        //Buch b6 = br.buchMitTitelFinden("Ein Freund fuer Nanoka");
-        //System.out.println(b2.autorAusgeben());
+        br.buchEinstellen(b1);
+        br.buchEinstellen(b2);
+        br.buchEinstellen(b3);
+        br.buchEinstellen(b4);
+        br.buchEinstellen(b5);
+        Buch b6 = br.buchMitTitelFinden("Ein Freund fuer Nanoka");
+        System.out.println(b2.autorAusgeben());
     }
 }
